a9706e89578f9ced9b28eebd63e92dc041ed0ccb
andreium1 <andreiu_m@yahoo.com>
2026-03-08 19:48:39 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,49 @@
+"use client"
 
+import { useState } from "react"
+import dares from "../dareloop_1000_dares.json"
 
 export default function Home() {
-  return (
-    <main style={{fontFamily:'sans-serif',padding:'40px',background:'#0b0b12',color:'white',minHeight:'100vh'}}>
-      <h1 style={{fontSize:'42px'}}>DARELOOP</h1>
-      <p>Truth or Dare – Web MVP</p>
-      <div style={{marginTop:'30px'}}>
-        <button style={{marginRight:'10px'}}>Party Mode</button>
-        <button style={{marginRight:'10px'}}>Daily Dare</button>
-        <button style={{marginRight:'10px'}}>Challenge Friend</button>
-        <button>Spicyspin</button>
-      </div>
-      <p style={{marginTop:'40px'}}>Site deployed successfully 🎉</p>
-    </main>
-  )
+
+const [dare, setDare] = useState("Press SPIN to get a dare 😈")
+
+function spinDare() {
+  const random = dares[Math.floor(Math.random()*dares.length)]
+  setDare(random.text)
+}
+
+return (
+  <main style={{
+    background:"#0f172a",
+    color:"white",
+    height:"100vh",
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+    fontFamily:"Arial"
+  }}>
+
+<h1 style={{fontSize:"40px"}}>DARELOOP</h1>
+
+<p style={{margin:"20px",fontSize:"20px",textAlign:"center"}}>
+{dare}
+</p>
+
+<button
+onClick={spinDare}
+style={{
+padding:"15px 40px",
+fontSize:"20px",
+background:"#22c55e",
+border:"none",
+borderRadius:"10px",
+cursor:"pointer"
+}}
+>
+🎲 SPIN
+</button>
+
+</main>
+)
 }
