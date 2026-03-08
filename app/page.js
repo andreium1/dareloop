32fc499032e9c2cdd5bd5321b23acaa883cb6d36
andreium1 <andreiu_m@yahoo.com>
2026-03-08 21:21:25 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -164,7 +164,7 @@ export default function Home() {
         >
           🎲 SPIN
         </button>
-
+        <VideoRecorder dare={dare} />
         <div
           style={{
             display: "flex",
