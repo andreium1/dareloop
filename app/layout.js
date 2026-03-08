f2e454fa3ebb5db0efc52e192d9ba32e36f0f37b
andreium1 <andreiu_m@yahoo.com>
2026-03-08 21:38:37 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
+import "./globals.css"
 
-
-export const metadata = { title: 'Dareloop' };
+export const metadata = { title: "Dareloop" }
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>{children}</body>
     </html>
-  );
+  )
 }
