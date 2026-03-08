e840c3562c036f2c5d551faf83174168f93704e8
andreium1 <andreiu_m@yahoo.com>
2026-03-08 18:14:26 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,11 @@
 
+
+export const metadata = { title: 'Dareloop' };
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
